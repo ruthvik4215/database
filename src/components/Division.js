ffe24d3581875ee4f2e65f77610d3343bd966e76
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-03-14 10:30:45 +0530

    some unessacery file deletions and some modifications.

---
+++ src/components/Division.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import './Division.css';
+
+const Division = () => {
+    return (
+        <div className="div-one">
+            <h1>Division One</h1>
+        </div>
+    )
+}
+
+export default divisions;
